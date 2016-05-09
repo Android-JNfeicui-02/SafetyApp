e55196c3ab5819e44ec3ac6d5732e2e2185341b8
Roy_sun <roy1993sun@gmail.com>
2016-05-09 11:35:05 +0800

    实现了ViewPager, pager的动画, 讲解了Pager里面的需要实现的接口.

--- app/src/main/java/com/roy/safeyapp/ui/MainActivity.java
+++ app/src/main/java/com/roy/safeyapp/ui/MainActivity.java
@@ -1,13 +1,15 @@
-package com.roy.safeyapp;
+package com.roy.safeyapp.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashActivity extends AppCompatActivity {
+import com.roy.safeyapp.R;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_main);
     }
 }
