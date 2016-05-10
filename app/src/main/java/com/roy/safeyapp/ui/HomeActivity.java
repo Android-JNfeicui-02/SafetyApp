becef6d12c5218808b7f03af37d4586615ee0c51
Roy_sun <roy1993sun@gmail.com>
2016-05-10 15:38:18 +0800

    GridView点击事件

--- app/src/main/java/com/roy/safeyapp/ui/HomeActivity.java
+++ app/src/main/java/com/roy/safeyapp/ui/HomeActivity.java
@@ -1,25 +1,36 @@
 package com.roy.safeyapp.ui;
 
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.roy.safeyapp.R;
 import com.roy.safeyapp.bean.HomeBean;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     private GridView mGridView;
 
+    Context mContext;
 
     private ArrayList<HomeBean> mDatas;
+    
+    private static final String TAG = "HomeActivity";
 
     private String[] desc = {"手机防盗", "通讯卫士",
                              "软件管理", "进程管理",
@@ -38,9 +49,26 @@ public class HomeActivity extends AppCompatActivity {
         initData();
 
 
+        PackageManager pm = this.getPackageManager();
+        List<PackageInfo> packages = pm.getInstalledPackages(PackageManager.GET_ACTIVITIES);
+        //Log.d(TAG, "PackageManager: " + packages.toString());
+
+
+        for (int i = 0; i < packages.size(); i++) {
+            PackageInfo pi = packages.get(i);
+            if ((pi.applicationInfo.flags & pi.applicationInfo.FLAG_SYSTEM) == 0) {
+                // 与运算出来的结果 如果不等于0 则是 系统内置的程序
+                // 如果等于0, 则是用户安装的程序
+                Log.d(TAG, "PackageInfo``: " + pi);
+            }
+        }
+
+
+    
         mGridView = (GridView) findViewById(R.id.gv_home);
         mGridView.setAdapter(new HomeAdapter());
 
+        mGridView.setOnItemClickListener(this);
 
 
     }
@@ -53,6 +81,18 @@ public class HomeActivity extends AppCompatActivity {
             bean.desc = desc[i];
             mDatas.add(bean);
         }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        switch (position) {
+            case 0:
+                Toast.makeText(HomeActivity.this, "点击了" + position, Toast.LENGTH_SHORT)
+                     .show();
+                break;
+        }
+
     }
 
     private class HomeAdapter extends BaseAdapter {
